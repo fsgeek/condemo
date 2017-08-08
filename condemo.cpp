3b2ff3a7fa8f9ae443cd977ca86dfb14bb43f9a4
Tony Mason <tony@wamason.com>
2017-08-07 22:34:49 -0700

    Initial buildable program

---
+++ condemo.cpp
@@ -0,0 +1,11 @@
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+int main(int argc, TCHAR **argv, TCHAR **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	printf("Hello World!");
+}
